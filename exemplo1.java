c7e4846d94cc0a3c3ec6173493ea2391d6051640
canavesi <carlos.canavesi@uol.com.br>
2020-09-24 08:44:42 -0300

    primeiras aulas Java

---
+++ exemplo1.java
@@ -0,0 +1,19 @@
+class exemplo1
+{
+      public static void main(final String args[]) {
+           final String nome = "Aprendendo  Java Prof. Neri 123";
+           final int num1 = 8;
+           final int num2 = 2;
+           final double num3 = 6.67;
+             System.out.println(nome);
+             System.out.println(num1);
+             System.out.println(num2);
+             System.out.println(num3);
+             System.out.println("A multiplicação de num1 pelo num2 é: "+(num1*num2));
+             System.out.println("A Divisão de num1 pelo num2 é: "+(num1/num2));
+             System.out.println("A soma de num1 pelo num2 é: "+(num1+num2));
+             System.out.println("A Subtração de num1 pelo num2 é: "+(num1-num2));
+             System.out.println("A multiplicação de num1 pelo num3 é: "+(num1*num3));
+        }
+
+}
---
+++ exemplo2.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+class exemplo2
+
+   {
+      public static void main(String args[])
+
+      {
+             JOptionPane.showMessageDialog(null, "Aprendendo Java");
+
+
+
+      }
+
+
+
+   }
+
+
