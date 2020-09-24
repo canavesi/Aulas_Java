9f306237aee1aea9f74e36464a6a0d9ffe4e57fb
canavesi <carlos.canavesi@uol.com.br>
2020-09-24 11:24:03 -0300

    exemplo de IF

---
+++ exemplo_if.java
@@ -0,0 +1,19 @@
+class exemplo_if
+
+{ public static void main(final String args[]) {
+        final double valor = 4.0;
+
+    if (valor >= 5)
+      System.out.println("O valor e maior ou igaul a 5");
+    else
+      System.out.println("O Valor é menor que 5");
+
+
+
+}
+
+
+
+
+
+}
