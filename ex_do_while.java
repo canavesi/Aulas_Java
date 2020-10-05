a570ce60825fa6e95523a5ce5818c27fc00e4e44
canavesi <carlos.canavesi@uol.com.br>
2020-10-05 15:59:13 -0300

    ex do while

---
+++ ex_do_while.java
@@ -0,0 +1,18 @@
+public class ex_do_while {
+    public static void main(String arg[])
+
+    {
+        int n = 1;
+        do
+        {
+            System.out.println(n);
+            n++;
+
+        }
+        while (n <= 10);
+
+    }
+
+
+
+}
