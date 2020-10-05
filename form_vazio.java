a0c50c753144733bcfe92d930dc0337a13a5c960
canavesi <carlos.canavesi@uol.com.br>
2020-10-05 17:15:09 -0300

    formulario vazio

---
+++ form_vazio.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import javax.swing.*;
+
+class form_vazio  extends JFrame
+{
+    form_vazio()
+        {
+            setTitle("Meu promeiro formulario");
+        }
+    public static void main(String args[])
+    {
+           JFrame janela = new form_vazio();
+           janela.show();
+     }
+}
