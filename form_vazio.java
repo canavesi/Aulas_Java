5f146f4b48bd6ed23bae2475fe0fe623c928e9a4
canavesi <carlos.canavesi@uol.com.br>
2020-10-18 09:43:47 -0300

    Inicio projeto estoque

--- form_vazio.java
+++ form_vazio.java
@@ -6,6 +6,11 @@ class form_vazio  extends JFrame
     form_vazio()
         {
             setTitle("Meu promeiro formulario");
+            setSize(800,400);
+            setLocation(50, 200);
+            setResizable(false);
+            getContentPane().setBackground(Color.red);
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         }
     public static void main(String args[])
     {
